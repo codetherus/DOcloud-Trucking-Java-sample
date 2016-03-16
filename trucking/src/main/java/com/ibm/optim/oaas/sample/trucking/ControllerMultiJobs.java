b0ba6285b2b647b7cb985cd0225254984ff6ac0a
mjaczyns <mjaczyns@us.ibm.com>
2016-03-16 10:48:40 -0700

    handling INFEASIBLE_SOLUTION and latest DOcloud client

--- trucking/src/main/java/com/ibm/optim/oaas/sample/trucking/ControllerMultiJobs.java
+++ trucking/src/main/java/com/ibm/optim/oaas/sample/trucking/ControllerMultiJobs.java
@@ -1,28 +1,22 @@
 package com.ibm.optim.oaas.sample.trucking;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.apache.http.impl.client.CloseableHttpClient;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.ibm.optim.oaas.client.OperationException;
 import com.ibm.optim.oaas.client.http.HttpClientFactory;
 import com.ibm.optim.oaas.client.job.AbstractJobCallback;
 import com.ibm.optim.oaas.client.job.JobClient;
 import com.ibm.optim.oaas.client.job.JobClientFactory;
-import com.ibm.optim.oaas.client.job.JobException;
 import com.ibm.optim.oaas.client.job.JobExecutor;
 import com.ibm.optim.oaas.client.job.JobExecutorFactory;
 import com.ibm.optim.oaas.client.job.JobLimitException;
@@ -58,56 +52,54 @@ public class ControllerMultiJobs {
    * Utility class to log jobs and optionally to update a latch.
    */
   public static class LogJobCallBack extends AbstractJobCallback {
-    private final String problemId;
+    private Problem problem;
+    private String problemId;
     private CountDownLatch latch;
+    private List<Problem> jobLimitProblemInstances;
+    private List<JobResponse> responses;
 
-    public LogJobCallBack(String problemId) {
-      this(problemId, null);
-    }
-
-    public LogJobCallBack(String problemId, CountDownLatch latch) {
-      this.problemId = problemId;
+    public LogJobCallBack(Problem problem, List<Problem> jobLimitProblemInstances, CountDownLatch latch, List<JobResponse> responses) {
+      this.problem = problem;
+      this.problemId = problem.getProblemId();
       this.latch=latch;
+      this.jobLimitProblemInstances=jobLimitProblemInstances;
+      this.responses=responses;
     }
 
     @Override
     public void created(JobResponse response) {
-      LOG.info("CREATED: " + problemId + " - " + response.getJobId());
     }
     @Override
     public void submitted(JobResponse response) {
-      LOG.info("SUBMITTED: " + problemId + " - " + response.getJobId());
     }
     @Override
     public void running(JobResponse response) {
-      LOG.info("RUNNING: " + problemId + " - " + response.getJobId());
     }
     @Override
     public void processed(JobResponse response) {
-      LOG.info("PROCESSED: " + problemId + " - " + response.getJobId());
     }
     @Override
     public void failed(JobResponse response) {
-      LOG.severe("FAILED: " + problemId + " - " + response.getJobId());
     }
     @Override
     public void interruption(JobResponse response) {
-      LOG.warning("INTERRUPTION: " + problemId + " - " + response.getJobId());
     }
+    
     @Override
     public void exception(JobResponse response, Exception e) {
       if (e instanceof JobLimitException) {
-        LOG.log(Level.WARNING, "The maximum job limit has been reached for the job associated with the problem: " + problemId);
+        LOG.log(Level.WARNING, "The maximum job limit has been reached for the job associated with the problem: " + problemId);        
       } else {
         LOG.severe("EXCEPTION: " + problemId + " - " + response.getJobId() + " : " + e.getLocalizedMessage());
       }
+      jobLimitProblemInstances.add(problem);
       if (latch!=null) latch.countDown();
     }
     @Override
-    public void completed(JobResponse response) {
+    public void completed(JobResponse response) {      
+      LOG.info("COMPLETED: " + problemId + " - " + response.getJobId());
+      responses.add(response);
       if (latch!=null) latch.countDown();
-      LOG.info("COMPLETED: " + problemId + " - " + response.getJobId() +
-          (latch != null ? "(number of jobs still running: " + latch.getCount() + ")" : ""));
     }
   }
 
@@ -124,7 +116,7 @@ public class ControllerMultiJobs {
 
     // Configure a job executor that can submit all jobs in parallel
     executor = JobExecutorFactory.custom()
-        .threads(nbJobs)
+        .threads(Math.min(nbJobs,10))
         .interval(1, TimeUnit.SECONDS).timeout(nbJobs, TimeUnit.MINUTES)
         .retry(4).retryDelay(5,TimeUnit.SECONDS)
         .build();
@@ -142,7 +134,7 @@ public class ControllerMultiJobs {
    * @param responses
    * @param latch
    */
-  public void submitJob(Problem pb, List<Future<JobResponse>> responses, List<Problem> jobLimitProblemInstances, CountDownLatch latch) {
+  public void submitJob(Problem pb, List<JobResponse> responses, List<Problem> jobLimitProblemInstances, CountDownLatch latch) {
     
     // Create a deserializer that uses the current problem instance as a context 
     // for resolving references to input data
@@ -162,14 +154,8 @@ public class ControllerMultiJobs {
     // completed to decrement the latch
     Future<JobResponse> response;
     try {
-      response = executor.execute(request, new LogJobCallBack(pb.getProblemId(), latch));
-      responses.add(response);
-
-    } catch (JobLimitException e) {
-      LOG.log(Level.WARNING, "Adding problem: " + pb.getProblemId() + " to the list of candidate problems to be re-submitted.");
-      jobLimitProblemInstances.add(pb);
-
-    } catch (JobException | OperationException | IOException | InterruptedException e) {
+      response = executor.execute(request, new LogJobCallBack(pb, jobLimitProblemInstances, latch, responses));      
+    } catch (Exception e) {
       LOG.log(Level.WARNING, "Error while executing a job",e);
     }
   }
@@ -178,39 +164,32 @@ public class ControllerMultiJobs {
    * Handle the response from a job that has been submitted asynchronously.
    * @param response
    */
-  public Solution processResponse(Future<JobResponse> response) {
-    // Make sure the executor thread has returned
-    try {
-      response.get(5,TimeUnit.SECONDS);
-
-      if (response.isDone()) {
-        JobResponse jobResponse = response.get();
+  public Solution processResponse(JobResponse response) {
+   
+        String problemId = response.getData().getParameters().getAsString(PARAM_PROBLEM_ID);
         
-        switch(jobResponse.getJob().getExecutionStatus()){
-        case PROCESSED:
-          List<? extends JobOutput> output = jobResponse.getOutput();
+        switch(response.getJob().getExecutionStatus()){
+        case PROCESSED:        	
+          List<? extends JobOutput> output = response.getOutput();
           Solution solution = (Solution) output.get(0).getContent();
-          String problemId = jobResponse.getData().getParameters().getAsString(PARAM_PROBLEM_ID);
-          LOG.info(problemId + " --> TOTAL COST = " + solution.getResult().getTotalCost()
-              + "  ================================================");
+          if (solution==null){
+        	  LOG.info(response.getJobId()+": "+problemId + " solve status is "+response.getJob().getSolveStatus());
+          } else {	         
+	          LOG.info(response.getJobId()+": "+problemId + " --> TOTAL COST = " + solution.getResult().getTotalCost());	         
+          }
           return solution;
 
         case FAILED:
           String message = "";
-          if (jobResponse.getJob().getFailureInfo()!=null){
-            message = jobResponse.getJob().getFailureInfo().getMessage();
+          if (response.getJob().getFailureInfo()!=null){
+            message = response.getJob().getFailureInfo().getMessage();
           }
-          System.out.println("Failed "+message);
+          LOG.warning(response.getJobId()+": "+"Failed "+message);
           break;
         default:
           break;        
         }
-      }
-
-    } catch (InterruptedException | ExecutionException | TimeoutException e) {
-      LOG.log(Level.WARNING, "An error was encountered while processing a job response",e);
-    }
-
+        
     return null;
   }
 
@@ -249,18 +228,13 @@ public class ControllerMultiJobs {
 		String apiKeyClientId = args[1];
 
     // Number of jobs to be submitted in parallel
-    final int NB_JOBS = 5;
+    final int NB_JOBS = 160;
 
     // Create the controller
     ControllerMultiJobs ctrl = new ControllerMultiJobs(baseURL, apiKeyClientId, NB_JOBS);
 
-    // List to hold the results
-    List<Future<JobResponse>> responses = new ArrayList<Future<JobResponse>>();
-
-    // Latch used to know when all the jobs have been processed
-    ProblemFactory pbFactory = new ProblemFactory();
-
     // Create all problem instances
+    ProblemFactory pbFactory = new ProblemFactory();
     List<Problem> problemInstances = new ArrayList<Problem>();
     for (int i = 0; i < NB_JOBS; i++) {
       // Create a problem instance
@@ -270,37 +244,35 @@ public class ControllerMultiJobs {
       problemInstances.add(pb);
     }
 
-    // Send all jobs to the server. If the job limit has been reached for a problem 
-    // instance, a new job will be created and submitted for that problem. There can 
-    // be up to NB_JOBS trials.
+    // List to hold the results of completed jobs
+    List<JobResponse> responses = new ArrayList<JobResponse>();
+    
+    // Send all jobs to the server by blocks, so that if job limit is reached,
+    // jobs will be retried in a next round.
     final int MAX_NB_TRIALS = NB_JOBS;
     int counterTrials = 0;
     while (!problemInstances.isEmpty() && (counterTrials++ < MAX_NB_TRIALS)) {
+    	
       final CountDownLatch latch = new CountDownLatch(problemInstances.size());
 
       // Send all jobs to the server
-      List<Problem> jobLimitProblemInstances = new ArrayList<Problem>();
+      List<Problem> problemsToRetry = new ArrayList<Problem>();
       for (Problem pb : problemInstances) {
-        ctrl.submitJob(pb, responses, jobLimitProblemInstances, latch);
+        ctrl.submitJob(pb, responses, problemsToRetry, latch);
       }
 
-      // Wait for all jobs to complete
+      // Wait for all jobs to compelete or raise exception
       try {
         latch.await();
       } catch (InterruptedException e) {
         LOG.log(Level.WARNING, "An error was encountered while waiting for job completion",e);
       }
 
-      problemInstances = jobLimitProblemInstances;
-    }
-
-    for (Problem pb : problemInstances) {
-      LOG.log(Level.SEVERE, "Could not solve problem: " + pb.getProblemId() + 
-              " because the maximum limit for the number of jobs has been reached. Please remove jobs to make space for new jobs.");
+      problemInstances = problemsToRetry;
     }
 
     // Check the results
-    for (Future<JobResponse> response : responses){
+    for (JobResponse response : responses){
       ctrl.processResponse(response);
     }
     
